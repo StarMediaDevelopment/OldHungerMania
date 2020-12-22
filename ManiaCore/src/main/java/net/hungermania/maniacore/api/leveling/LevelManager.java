faa025a56270ef9bc948dbfad0a9b51e859eb997
BuildTools <unconfigured@null.spigotmc.org>
2020-12-22 00:31:14 -0600

    Rebalanced the levels

--- ManiaCore/src/main/java/net/hungermania/maniacore/api/leveling/LevelManager.java
+++ ManiaCore/src/main/java/net/hungermania/maniacore/api/leveling/LevelManager.java
@@ -3,11 +3,10 @@ package net.hungermania.maniacore.api.leveling;
 import net.hungermania.maniacore.api.ManiaCore;
 import net.hungermania.maniacore.api.records.LevelRecord;
 import net.hungermania.manialib.sql.IRecord;
+import net.hungermania.manialib.util.Range;
 import net.md_5.bungee.api.ChatColor;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class LevelManager {
     private final ManiaCore maniaCore;
@@ -32,28 +31,34 @@ public class LevelManager {
     
     public void generateDefaults() {
         if (levels.isEmpty()) {
-            Level levelOne = new Level(1, 0, 100, ChatColor.GRAY);
-            Level levelTwo = new Level(2, 50, 0, ChatColor.GRAY);
-            Level levelThree = new Level(3, 100, 0, ChatColor.GRAY);
-            Level levelFour = new Level(4, 200, 0, ChatColor.GRAY);
-            Level levelFive = new Level(5, 400, 0, ChatColor.GRAY);
-            Level levelSix = new Level(6, 800, 0, ChatColor.GRAY);
-            Level levelSeven = new Level(7, 1600, 0, ChatColor.GRAY);
-            Level levelEight = new Level(8, 3200, 0, ChatColor.GRAY);
-            Level levelNine = new Level(9, 6400, 0, ChatColor.GRAY);
-            Level levelTen = new Level(10, 12800, 3000, ChatColor.DARK_AQUA);
-            Level levelEleven = new Level(11, 14080, 0, ChatColor.DARK_AQUA);
-            Level levelTwelve = new Level(12, 15488, 0, ChatColor.DARK_AQUA);
-            Level levelThirteen = new Level(13, 17036, 0, ChatColor.DARK_AQUA);
-            Level levelFourteen = new Level(14, 18739, 0, ChatColor.DARK_AQUA);
-            Level levelFifteen = new Level(15, 20612, 0, ChatColor.DARK_AQUA);
-            Level levelSixteen = new Level(16, 22673, 0, ChatColor.DARK_AQUA);
-            Level levelSeventeen = new Level(17, 24940, 0, ChatColor.DARK_AQUA);
-            Level levelEighteen = new Level(18, 27434, 0, ChatColor.DARK_AQUA);
-            Level levelNineteen = new Level(19, 30177, 0, ChatColor.DARK_AQUA);
-            Level levelTwenty = new Level(20, 33194, 10000, ChatColor.GOLD);
-            generateLevelDatabase(levelOne, levelTwo, levelThree, levelFour, levelFive, levelSix, levelSeven, levelEight, levelNine, levelTen, levelEleven, levelTwelve,
-                    levelThirteen, levelFourteen, levelFifteen, levelSixteen, levelSeventeen, levelEighteen, levelNineteen, levelTwenty);
+            int totalXp = 5000;
+    
+            Set<Range<ChatColor>> levelColors = new HashSet<>();
+            levelColors.add(new Range<>(1, 9, ChatColor.GRAY));
+            levelColors.add(new Range<>(10, 19, ChatColor.DARK_AQUA));
+            levelColors.add(new Range<>(20, 29, ChatColor.YELLOW));
+            levelColors.add(new Range<>(30, 39, ChatColor.BLUE));
+            levelColors.add(new Range<>(40, 49, ChatColor.AQUA));
+            levelColors.add(new Range<>(50, 59, ChatColor.GREEN));
+            levelColors.add(new Range<>(60, 69, ChatColor.RED));
+            levelColors.add(new Range<>(70, 79, ChatColor.DARK_PURPLE));
+            levelColors.add(new Range<>(80, 89, ChatColor.DARK_RED));
+            levelColors.add(new Range<>(90, 99, ChatColor.LIGHT_PURPLE));
+            levelColors.add(new Range<>(100, 100, ChatColor.GOLD));
+            
+            for (int i = 1; i <= 100; i++) {
+                ChatColor color = ChatColor.GRAY;
+    
+                for (Range<ChatColor> range : levelColors) {
+                    if (range.contains(i)) {
+                        color = range.getObject();
+                    }
+                }
+                
+                Level level = new Level(i, i * 5000, 0, color);
+                ManiaCore.getInstance().getDatabase().addRecordToQueue(new LevelRecord(level));
+                this.levels.put(i, level);
+            }
         }
     }
     
