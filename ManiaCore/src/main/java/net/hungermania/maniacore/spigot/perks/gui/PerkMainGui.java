44be4e31ccdc4c07242616d87576e1ba86b92110
Firestar311 <firestar311mc@gmail.com>
2021-01-04 23:07:40 -0600

    Fixed click types not working properly

--- ManiaCore/src/main/java/net/hungermania/maniacore/spigot/perks/gui/PerkMainGui.java
+++ ManiaCore/src/main/java/net/hungermania/maniacore/spigot/perks/gui/PerkMainGui.java
@@ -1,11 +1,12 @@
 package net.hungermania.maniacore.spigot.perks.gui;
 
+import net.hungermania.maniacore.api.ManiaCore;
 import net.hungermania.maniacore.spigot.gui.GUIButton;
 import net.hungermania.maniacore.spigot.gui.Gui;
-import net.hungermania.maniacore.spigot.perks.Perk;
-import net.hungermania.maniacore.spigot.perks.Perks;
+import net.hungermania.maniacore.spigot.perks.*;
 import net.hungermania.maniacore.spigot.user.SpigotUser;
 import org.bukkit.Bukkit;
+import org.bukkit.event.inventory.ClickType;
 
 public class PerkMainGui extends Gui {
     public PerkMainGui(SpigotUser user) {
@@ -13,8 +14,14 @@ public class PerkMainGui extends Gui {
     
         for (Perk perk : Perks.PERKS) {
             GUIButton button = new GUIButton(perk.getIcon(user));
-            button.setListener(e -> { 
-                perk.handlePurchase(user); 
+            button.setListener(e -> {
+                if (e.getClick() == ClickType.LEFT) {
+                    perk.handlePurchase(user);
+                } else if (e.getClick() == ClickType.RIGHT) {
+                    PerkInfo perkInfo = ((SpigotUser) ManiaCore.getInstance().getUserManager().getUser(e.getWhoClicked().getUniqueId())).getPerkInfo(perk);
+                    perkInfo.setActive(true);
+                    new PerkInfoRecord(perkInfo).push(ManiaCore.getInstance().getDatabase());
+                }
                 refreshInventory(e.getWhoClicked());
             });
             addButton(button);
