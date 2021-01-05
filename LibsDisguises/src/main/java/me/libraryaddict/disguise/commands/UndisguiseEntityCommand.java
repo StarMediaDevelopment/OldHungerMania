7b51ac882d64ca41fc5d1742897d062dec04743e
Firestar311 <firestar311mc@gmail.com>
2021-01-05 00:20:11 -0600

    Move to using a fork of LibsDisguises instead of relying on a jar file

---
+++ LibsDisguises/src/main/java/me/libraryaddict/disguise/commands/UndisguiseEntityCommand.java
@@ -0,0 +1,23 @@
+package me.libraryaddict.disguise.commands;
+
+import me.libraryaddict.disguise.LibsDisguises;
+import org.bukkit.ChatColor;
+import org.bukkit.command.*;
+
+public class UndisguiseEntityCommand implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender.getName().equals("CONSOLE")) {
+            sender.sendMessage(ChatColor.RED + "You may not use this command from the console!");
+            return true;
+        }
+        if (sender.hasPermission("libsdisguises.undisguiseentity")) {
+            LibsDisguises.instance.getListener().setDisguiseEntity(sender.getName(), null);
+            sender.sendMessage(ChatColor.RED + "Right click a disguised entity to undisguise them!");
+        } else {
+            sender.sendMessage(ChatColor.RED + "You are forbidden to use this command.");
+        }
+        return true;
+    }
+}
