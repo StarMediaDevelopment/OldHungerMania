f458cc2fb7c4e1d78ac2aa935b4a4e15533ea8ff
Firestar311 <firestar311mc@gmail.com>
2021-01-25 19:48:29 -0600

    Added a class to represent game overrides

---
+++ HungerGames/src/main/java/net/hungermania/hungergames/lobby/GameOverrides.java
@@ -0,0 +1,13 @@
+package net.hungermania.hungergames.lobby;
+
+public class GameOverrides {
+    /*
+     * Map - Top row, back, then the current maps, then a clock that has information for all maps
+     * Grace Period - clicking toggles between 60, 45, 30, 15, off
+     * Auto-Graceperiod (Maybe)
+     * Sponsoring (When implemented)
+     * Mutations
+     * Health (Starts at default then, left is increase, right is decrease)
+     * Other Game Settings Except - Max and Min players, startTimer, max games, map options
+     */
+}
