edb01e52f6586cc4441aa6767381ea5de44a3d7d
Firestar311 <firestar311mc@gmail.com>
2021-02-16 23:18:20 -0600

    Added some utilities

--- ManiaLib/src/main/java/net/hungermania/manialib/util/Utils.java
+++ ManiaLib/src/main/java/net/hungermania/manialib/util/Utils.java
@@ -17,9 +17,9 @@ public final class Utils {
     public static void printCurrentStack() {
         System.out.println(Arrays.toString(new Throwable().getStackTrace()));
     }
-    
+
     private static Map<Class<?>, Set<Field>> cachedFields = new HashMap<>();
-    
+
     public static Set<Field> getClassFields(Class<?> clazz) {
         if (cachedFields.containsKey(clazz)) {
             return cachedFields.get(clazz);
@@ -35,7 +35,42 @@ public final class Utils {
         }
         return fields;
     }
-    
+
+    public static String romanNumerals(int Int) {
+        LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<>();
+        roman_numerals.put("M", 1000);
+        roman_numerals.put("CM", 900);
+        roman_numerals.put("D", 500);
+        roman_numerals.put("CD", 400);
+        roman_numerals.put("C", 100);
+        roman_numerals.put("XC", 90);
+        roman_numerals.put("L", 50);
+        roman_numerals.put("XL", 40);
+        roman_numerals.put("X", 10);
+        roman_numerals.put("IX", 9);
+        roman_numerals.put("V", 5);
+        roman_numerals.put("IV", 4);
+        roman_numerals.put("I", 1);
+        String res = "";
+        for (Map.Entry<String, Integer> entry : roman_numerals.entrySet()) {
+            int matches = Int / entry.getValue();
+            res += repeat(entry.getKey(), matches);
+            Int = Int % entry.getValue();
+        }
+        return res;
+    }
+
+    public static String repeat(String s, int n) {
+        if (s == null) {
+            return null;
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
     public static Set<Field> getClassFields(Class<?> clazz, Set<Field> fields) {
         if (cachedFields.containsKey(clazz)) {
             fields.addAll(cachedFields.get(clazz));
@@ -44,7 +79,7 @@ public final class Utils {
         if (fields == null) {
             fields = new LinkedHashSet<>();
         }
-        
+
         fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
         if (clazz.getSuperclass() != null) {
             getClassFields(clazz.getSuperclass(), fields);
@@ -56,7 +91,7 @@ public final class Utils {
         }
         return fields;
     }
-    
+
     public static Calendar parseCalendarDate(String rawDate) throws InvalidDateFormatException {
         short[] dateValues = Utils.parseDate(rawDate);
         Calendar calendar = Calendar.getInstance();
@@ -68,27 +103,27 @@ public final class Utils {
         calendar.set(Calendar.SECOND, dateValues[5]);
         return calendar;
     }
-    
+
     public static short[] parseDate(String rawDate) throws InvalidDateFormatException {
         String[] rawDateArray = rawDate.split("/");
         if (!(rawDateArray.length >= 3)) {
             throw new InvalidDateFormatException("Invalid day of year arguments");
         }
         short month = Utils.parseTimeArgument(rawDateArray[0], "month"), day = Utils.parseTimeArgument(rawDateArray[1], "day"), year = Utils.parseTimeArgument(rawDateArray[2], "year");
-        
+
         if (month == -2 || day == -2 || year == -2) {
             return null;
         }
-        
+
         short hour, minute, second;
-        
+
         if (rawDateArray.length == 4) {
             String[] rawTimeArray = rawDateArray[3].split(":");
-            
+
             hour = Utils.parseTimeArgument(rawTimeArray[0], "hour");
             minute = Utils.parseTimeArgument(rawTimeArray[1], "minute");
             second = Utils.parseTimeArgument(rawTimeArray[2], "second");
-            
+
             if (hour == -2 || minute == -2 || second == -2) {
                 return null;
             }
@@ -97,10 +132,10 @@ public final class Utils {
             minute = -1;
             second = -1;
         }
-        
+
         return new short[]{month, day, year, hour, minute, second};
     }
-    
+
     private static short parseTimeArgument(String arg, String type) {
         try {
             return Short.parseShort(arg);
@@ -108,18 +143,18 @@ public final class Utils {
             return -2;
         }
     }
-    
+
     public static String formatDate(long time) {
         if (time == 0) {
             return "0";
         }
         return Constants.DATE_FORMAT.format(new Date(time));
     }
-    
+
     public static String formatNumber(Number number) {
         return Constants.NUMBER_FORMAT.format(number);
     }
-    
+
     public static String capitalizeEveryWord(String string) {
         string = string.toLowerCase();
         String[] words = string.split("_");
@@ -139,10 +174,10 @@ public final class Utils {
                 name.append(" ");
             }
         }
-        
+
         return name.toString();
     }
-    
+
     public static String join(Collection collection, String separator) {
         Iterator iterator = collection.iterator();
         if (iterator == null) {
@@ -158,27 +193,27 @@ public final class Utils {
                 if (first != null) {
                     buf.append(first);
                 }
-                
+
                 while (iterator.hasNext()) {
                     if (separator != null) {
                         buf.append(separator);
                     }
-                    
+
                     Object obj = iterator.next();
                     if (obj != null) {
                         buf.append(obj);
                     }
                 }
-                
+
                 return buf.toString();
             }
         }
     }
-    
+
     public static boolean isEmpty(String str) {
         return str == null || str.length() == 0;
     }
-    
+
     public static void copyFolder(Path src, Path dest) {
         try (Stream<Path> stream = Files.walk(src)) {
             stream.forEach(source -> copy(source, dest.resolve(src.relativize(source))));
@@ -186,7 +221,7 @@ public final class Utils {
             e.printStackTrace();
         }
     }
-    
+
     public static String formatTime(long time) {
         Duration remainingTime = Duration.ofMillis(time);
         long days = remainingTime.toDays();
@@ -196,13 +231,21 @@ public final class Utils {
         long minutes = remainingTime.toMinutes();
         remainingTime = remainingTime.minusMinutes(minutes);
         long seconds = remainingTime.getSeconds();
-        
+
         StringBuilder sb = new StringBuilder();
-        if (days > 0) { sb.append(days).append("d"); }
-        if (hours > 0) { sb.append(hours).append("h"); }
-        if (minutes > 0) { sb.append(minutes).append("m"); }
-        if (seconds > 0) { sb.append(seconds).append("s"); }
-        
+        if (days > 0) {
+            sb.append(days).append("d");
+        }
+        if (hours > 0) {
+            sb.append(hours).append("h");
+        }
+        if (minutes > 0) {
+            sb.append(minutes).append("m");
+        }
+        if (seconds > 0) {
+            sb.append(seconds).append("s");
+        }
+
         if (sb.toString().isEmpty()) {
             sb.append("0s");
         }
@@ -212,7 +255,7 @@ public final class Utils {
 //        }
         return sb.toString();
     }
-    
+
     private static void copy(Path source, Path dest) {
         try {
             Files.copy(source, dest, REPLACE_EXISTING);
@@ -220,18 +263,20 @@ public final class Utils {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
-    
+
     public static void purgeDirectory(File dir) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
-                if (file.isDirectory()) { purgeDirectory(file); }
+                if (file.isDirectory()) {
+                    purgeDirectory(file);
+                }
                 file.delete();
             }
         }
         dir.delete();
     }
-    
+
     public static String niceTime(int seconds) {
         int hours = seconds / 3600;
         seconds -= hours * 3600;
@@ -239,7 +284,7 @@ public final class Utils {
         seconds -= minutes * 60;
         return niceTime(hours, minutes, seconds);
     }
-    
+
     public static String niceTime(int seconds, boolean showEmptyHours) {
         int hours = seconds / 3600;
         seconds -= hours * 3600;
@@ -247,11 +292,11 @@ public final class Utils {
         seconds -= minutes * 60;
         return niceTime(hours, minutes, seconds, showEmptyHours);
     }
-    
+
     public static String niceTime(int hours, int minutes, int seconds) {
         return niceTime(hours, minutes, seconds, true);
     }
-    
+
     public static String niceTime(int hours, int minutes, int seconds, boolean showEmptyHours) {
         StringBuilder builder = new StringBuilder();
 
